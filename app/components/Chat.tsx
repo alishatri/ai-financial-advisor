b1c08dfe83e17073715da9b6e59507ef659ab70d
Team A <alishatri21@gmail.com>
2023-10-14 12:27:58 +0200

    chat update

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -1,43 +1,57 @@
-import { useEffect, useState } from "react";
-import { useChat } from "ai/react";
-import ChatBox from "react-chat-plugin";
+import { useEffect, useState, ComponentType } from 'react';
+import { useChat } from 'ai/react';
+import dynamic from 'next/dynamic';
 
 type ChatMessage = {
   author: {
     username: string;
     id: number;
     avatarUrl: string;
-  };
+  },
   text: string;
   type: string;
   timestamp: number;
 };
 
+type ChatBoxProps = {
+  messages: ChatMessage[];
+  userId: number;
+  onSendMessage: (message: string) => void;
+  width?: string;
+  height?: string;
+  style?: React.CSSProperties;
+};
+
+const ChatBox = dynamic<ChatBoxProps>(
+  () =>
+    import('react-chat-plugin').then(
+      (mod) => mod.default as ComponentType<ChatBoxProps>
+    ),
+  { loading: () => null, ssr: false }
+);
+
 const userAuthor = {
-  username: "User",
+  username: 'User',
   id: 1,
-  avatarUrl: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+  avatarUrl: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
 };
 
 const aiAuthor = {
-  username: "AI",
+  username: 'AI',
   id: 2,
-  avatarUrl:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxG04HIgwI_xov-4yMXrMZjR9Iw3k3VdYDww&usqp=CAU",
+  avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxG04HIgwI_xov-4yMXrMZjR9Iw3k3VdYDww&usqp=CAU',
 };
 
 const Chat = () => {
   const initialMessage = {
     author: aiAuthor,
     text: "Hey, how can I help you?",
-    type: "text",
+    type: 'text',
     timestamp: +new Date(),
   };
-  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([
-    initialMessage,
-  ]);
+  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([initialMessage]);
   const { append, messages } = useChat({
-    api: "/api/openai",
+    api: '/api/openai',
   });
 
   useEffect(() => {
@@ -47,14 +61,14 @@ const Chat = () => {
       system: aiAuthor,
       function: aiAuthor,
       assistant: aiAuthor,
-    };
-    const chatMessagesArr = messages?.map((message) => {
-      return {
+    }
+    const chatMessagesArr = messages?.map(message => {
+      return ({
         author: authors[message.role],
         text: message?.content,
-        type: "text",
+        type: 'text',
         timestamp: +new Date(),
-      };
+      });
     });
     setChatMessages([initialMessage, ...chatMessagesArr]);
   }, [messages]);
@@ -62,25 +76,20 @@ const Chat = () => {
   const handleOnSendMessage = (message: string) => {
     append({
       content: message,
-      role: "user",
+      role: 'user'
     });
-  };
+  }
 
   return (
-    <>
-      <ChatBox
-        style={{
-          margin: "auto",
-          whiteSpace: "pre-wrap",
-        }}
-        messages={chatMessages}
-        userId={1}
-        onSendMessage={handleOnSendMessage}
-        width={"550px"}
-        height={"500px"}
-      />
-    </>
+    <ChatBox
+      style={{margin: 'auto'}}
+      messages={chatMessages}
+      userId={1}
+      onSendMessage={handleOnSendMessage}
+      width={'550px'}
+      height={'500px'}
+    />
   );
-};
+}
 
 export default Chat;
